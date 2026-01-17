c643edabbd49d0613b925eff615b7c10c53f35bb
MrVinayakGupta <163561684+MrVinayakGupta@users.noreply.github.com>
2026-01-18 00:34:25 +0530

    Update review.js

--- routes/review.js
+++ routes/review.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
-const Listing = require("../models/listing.js");
 const Review = require("../models/review.js");
 const { listingSchema, reviewSchema } = require("../schema.js");
 
