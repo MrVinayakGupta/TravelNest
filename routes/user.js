eedb3a1479b19bb9e1aa904135d79c5570663271
MrVinayakGupta <163561684+MrVinayakGupta@users.noreply.github.com>
2026-01-22 18:47:04 +0530

    Singup user-post with lettel error

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const user = require("../models/user.js");
-const Listing = require("../models/listing.js");
+const wrapAsync = require("../utils/wrapAsync.js");
 
 router.get("/singup", (req, res) => {
     res.render("ejs/singup.ejs");
 });
+
+router.post("/singup", wrapAsync( async (req, res, next) => {
+    try {
+        let { username, email, password } = req.body;
+        const newUser = new user({ username, email });
+        const registeredUser = await user.register(newUser, password);
+        console.log(registeredUser);
+        req.flash("success", "Welcome to TravelNest!");
+        res.redirect("/");
+    }catch(e) {
+        req.flash("error", e.message);
+        res.redirect("/singup");
+    }
+}));
 
 module.exports = router;
