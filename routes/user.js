970a0143229c8bc8dac1e3b9937686f2967bad62
MrVinayakGupta <163561684+MrVinayakGupta@users.noreply.github.com>
2026-01-24 15:41:15 +0530

    logout user

--- routes/user.js
+++ routes/user.js
@@ -31,4 +31,15 @@ router.post("/login", passport.authenticate("local",{failureRedirect: "/login", 
     res.redirect("/");
 });
 
+router.get("/logout", (req, res, next) => {
+    req.logout( (err) => {
+        if (err) {
+            return next(err);
+        }
+
+        req.flash("success", "You are logged out successfully!");
+        res.redirect("/");
+    });
+});
+
 module.exports = router;
