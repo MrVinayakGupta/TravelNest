06a1f4b3f92a31a178fd63de07225514b095c33c
MrVinayakGupta <163561684+MrVinayakGupta@users.noreply.github.com>
2026-01-30 02:37:24 +0530

    Authorization for /listings

--- routes/listing.js
+++ routes/listing.js
@@ -5,7 +5,7 @@ const ExpressError = require("../utils/ExpressError.js");
 const Listing = require("../models/listing.js");
 const {listingSchema} = require("../schema.js");
 const {reviewSchema} = require("../schema.js");
-const {isLoggedIn} = require("../middleware.js"); 
+const {isLoggedIn, isOwner} = require("../middleware.js"); 
 
 const validateListing = (req, res, next) => {
     let {error} = listingSchema.validate(req.body);
@@ -61,24 +61,29 @@ router.post("/", isLoggedIn, validateListing, wrapAsync( async (req, res, next) 
 }));
 
 // Edit Route
-router.get("/:id/edit", isLoggedIn, wrapAsync( async (req, res) => {
+router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync( async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
+    if(!listing) {
+        req.flash("error", "Cannot find that listing!");
+        return res.redirect("/");
+    }
     res.render("ejs/edit.ejs", { id, listing });
 }));
 
 //Update Route
-router.put("/:id", isLoggedIn, validateListing, wrapAsync( async (req, res) => {
+router.put("/:id", isLoggedIn, isOwner, validateListing, wrapAsync( async (req, res) => {
     if(!req.body.listing) {
         throw new ExpressError(400, "Send valid data for listing");
     }
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    req.flash("success", "Successfully updated the listing!");
     res.redirect(`/${id}`);
 }));
 
 //Delete Route
-router.delete("/:id", isLoggedIn, wrapAsync( async (req, res) => {
+router.delete("/:id", isLoggedIn, isOwner, wrapAsync( async (req, res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
