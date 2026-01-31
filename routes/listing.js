c823046f529d0712d9cbd789afdc872f4a654bbe
MrVinayakGupta <163561684+MrVinayakGupta@users.noreply.github.com>
2026-01-31 18:41:04 +0530

    Update listing.js

--- routes/listing.js
+++ routes/listing.js
@@ -27,7 +27,7 @@ router.get('/favicon.ico', (req, res) => res.status(204).end());
 //Show Route
 router.get("/:id", wrapAsync( async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id).populate("reviews").populate("owner");
+    const listing = await Listing.findById(id).populate({path:"reviews", populate: {path: "author"},} ).populate("owner");
     
     if(!listing) {
         req.flash("error", "Cannot find that listing!");
