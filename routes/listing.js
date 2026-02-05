94d23eb3783f5bf5a6a89b5ccd50cb138325deaa
MrVinayakGupta <163561684+MrVinayakGupta@users.noreply.github.com>
2026-02-05 16:17:42 +0530

    store file

--- routes/listing.js
+++ routes/listing.js
@@ -5,7 +5,9 @@ const Listing = require("../models/listing.js");
 const {isLoggedIn, isOwner, validateListing} = require("../middleware.js"); 
 
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
+
 
 const listingController = require("../controllers/listing.js");
 
